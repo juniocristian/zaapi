0763ebb30fb596ed014de882ee32fb875c77a10f
juniocristian <juniocristianmg@gmail.com>
2023-10-12 11:57:25 -0300

    s

--- frontend/src/translate/languages/pt.js
+++ frontend/src/translate/languages/pt.js
@@ -291,8 +291,8 @@ const messages = {
         noTicketsMessage:
           "Nenhum atendimento encontrado com esse status ou termo pesquisado",
         buttons: {
-          accept: "Aceitar",
-          closed: "Finalizar",
+          accept: "✔",
+          closed: "✖",
           reopen: "Reabrir"
         },
       },
