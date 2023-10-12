4996f0457fc6528054635aaa0d6979004585d00d
juniocristian <juniocristianmg@gmail.com>
2023-10-12 09:42:06 -0300

    Update index.js

--- frontend/src/components/TicketListItemCustom/index.js
+++ frontend/src/components/TicketListItemCustom/index.js
@@ -506,7 +506,24 @@ const useStyles = makeStyles((theme) => ({
           {(ticket.status !== "closed") && (
             <ButtonWithSpinner
               //color="primary"
-              style={{ backgroundColor: '#2a6a5a', color: 'white', padding: '0px', bottom: '54px', borderRadius: '50px', left: '300px', fontSize: '0.6rem' }}
+              import React from 'react';
+
+const SeuComponenteReact = () => {
+  // Substitua $seuCodigoSvg pelo seu código SVG
+  const seuCodigoSvg = '<path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"></path>';
+
+  const svgCode = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">${seuCodigoSvg}</svg>`;
+
+  return (
+    <div>
+      {/* Renderizar seu código SVG React */}
+      <div dangerouslySetInnerHTML={{ __html: svgCode }} />
+    </div>
+  );
+};
+
+export default SeuComponenteReact;
+
               variant="contained"
               className={classes.acceptButton}
               size="small"
